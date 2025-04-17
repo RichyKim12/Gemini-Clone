f3a7e6e9b0ab14281edff574f7695f26be171fed
RichyKim12 <109635077+RichyKim12@users.noreply.github.com>
2025-04-16 20:59:09 -0400

    gemini api functionality + UI

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+
 import { assets } from '../../assets/assets'
 import './Main.css'
+import { Context } from '../../context/Context';
 
 const Main = () => {
+  const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context);
   return (
     <div className="main">
         <div className="nav">
@@ -10,35 +13,58 @@ const Main = () => {
             <img src={assets.user_icon}/>
         </div>
         <div className="main-container">
-            <div className="greet">
-                <p><span>Hello Dav</span></p>
-                <p>How can I help you today</p>
+            {!showResult?
+            <>
+                <div className="greet">
+                    <p><span>Hello Dav</span></p>
+                    <p>How can I help you today</p>
+                </div>
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful places to see on an upcoming trip</p>
+                        <img src={assets.compass_icon} />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summarize this concept: urban planning</p>
+                        <img src={assets.bulb_icon} />
+                    </div>
+                    <div className="card">
+                        <p>Brainstorm team bonding activities for our work retreat</p>
+                        <img src={assets.message_icon} />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} />
+                    </div>
+                </div>
+            </>
+            :
+            <div className = "result">
+                <div className = "result-title">
+                    <img src={assets.user_icon} />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon}/>
+                    {loading?
+                    <div className="loader">
+                        <hr/>
+                        <hr/>
+                        <hr/>
+                    </div>
+                    :<div><p dangerouslySetInnerHTML={{__html:resultData}}></p></div>}
+                    
+                </div>
             </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming trip</p>
-                    <img src={assets.compass_icon} />
-                </div>
-                <div className="card">
-                    <p>Briefly summarize this concept: urban planning</p>
-                    <img src={assets.bulb_icon} />
-                </div>
-                <div className="card">
-                    <p>Brainstorm team bonding activities for our work retreat</p>
-                    <img src={assets.message_icon} />
-                </div>
-                <div className="card">
-                    <p>Improve the readability of the following code</p>
-                    <img src={assets.code_icon} />
-                </div>
-            </div>
+            }
+            
             <div className="main-bottom">
                 <div className="search-box">
-                    <input type="text" placeholder="Enter a prompt here "/>
+                    <input onChange={(e)=>setInput(e.target.value)} value ={input} type="text" placeholder="Enter a prompt here "/>
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+                        <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
                     </div>
                 </div>
                 <p className="bottom-info">
